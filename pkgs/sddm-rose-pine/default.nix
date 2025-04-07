19f9d746aae2d5f2b8c51ec1407cad8a187a9ed3
Julia Mertz <info@juliamertz.dev>
2025-04-07 22:02:17 +0200

    cleanup

--- pkgs/sddm-rose-pine/default.nix
+++ pkgs/sddm-rose-pine/default.nix
@@ -2,38 +2,40 @@
   sddm-rose-pine,
   libsForQt5,
 }:
-libsForQt5.callPackage (
-  {
-    lib,
-    qtbase,
-    qtsvg,
-    qtgraphicaleffects,
-    qtquickcontrols2,
-    wrapQtAppsHook,
-    stdenvNoCC,
-  }:
-  stdenvNoCC.mkDerivation {
-    pname = "sddm-rose-pine";
-    version = "1.0.0";
-    dontBuild = true;
-    src = sddm-rose-pine;
+let
+  pkg =
+    {
+      lib,
+      qtbase,
+      qtsvg,
+      qtgraphicaleffects,
+      qtquickcontrols2,
+      wrapQtAppsHook,
+      stdenvNoCC,
+    }:
+    stdenvNoCC.mkDerivation {
+      pname = "sddm-rose-pine";
+      version = "1.0.0";
+      dontBuild = true;
+      src = sddm-rose-pine;
 
-    nativeBuildInputs = [ wrapQtAppsHook ];
-    propagatedUserEnvPkgs = [
-      qtbase
-      qtsvg
-      qtgraphicaleffects
-      qtquickcontrols2
-    ];
+      nativeBuildInputs = [ wrapQtAppsHook ];
+      propagatedUserEnvPkgs = [
+        qtbase
+        qtsvg
+        qtgraphicaleffects
+        qtquickcontrols2
+      ];
 
-    installPhase = ''
-      mkdir -p $out/share/sddm/themes
-      cp -aR $src $out/share/sddm/themes/rose-pine
-    '';
+      installPhase = ''
+        mkdir -p $out/share/sddm/themes
+        cp -aR $src $out/share/sddm/themes/rose-pine
+      '';
 
-    meta = {
-      homepage = "https://github.com/juliamertz/sddm-rose-pine";
-      platforms = lib.platforms.linux;
+      meta = {
+        homepage = "https://github.com/juliamertz/sddm-rose-pine";
+        platforms = lib.platforms.linux;
+      };
     };
-  }
-) { }
+in
+libsForQt5.callPackage pkg { }
